30517cf53fc86e5eeb46bece04a0b53f2fa77ee6
AnzalnaKainat <anzalnakainat@gmail.com>
2024-01-12 21:09:56 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// // navigation
+function showMenu() {
+  var navLinks = document.getElementById("navLinks");
+  navLinks.classList.toggle("active");
+}
+
+function hideMenu() {
+  var navLinks = document.getElementById("navLinks");
+  navLinks.classList.remove("active");
+}
+
+// Pages Links
+function LandingPage(){
+  window.location.href = 'index.html';
+}
+function GoToSignUpPage(){
+  window.location.href = 'signup.html';
+}
+
+function GoToLoginPage(){
+  window.location.href = 'login.html';
+}
+function GoToHomePage(){
+  window.location.href="homepage.html";
+}
+function ToGoScholarship(){
+  window.location.href="scholarship.html";
+}
+function EnrolledCourseInfo(){
+  window.location.href="enrolled-course-info.html";
+}
+function GoToCourseMaterial(){
+  window.location.href = "course-material.html";
+}
+function GoToCompletionCourse(){
+  window.location.href="completion-course.html";
+}
+
+function GoToCourseDuration(){
+  window.location.href = "course-duration.html";
+}
+/////////////////////////////////////////////////Dicussion Forum////////////////////////////////////////////////////////////
